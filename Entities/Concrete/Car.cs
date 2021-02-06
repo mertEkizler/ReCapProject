167ec41e527451328930fe35ac81ba18c2481e8e
mert ekizler <mertekizler@hotmail.com>
2021-02-06 15:11:27 +0300

    .

--- Entities/Concrete/Car.cs
+++ Entities/Concrete/Car.cs
@@ -5,15 +5,13 @@ using System.Text;
 
 namespace Entities.Concrete
 {
-    public class Car:IEntity
+    public class Car : IEntity
     {
         public int CarId { get; set; }
-        public string CarName { get; set; }
+        public string ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Descriptions { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
-        public int ModelYear { get; set; }
-        public decimal DailyPrice { get; set; }
-        public string Description { get; set; }
-
     }
 }
---
+++ SQLQuery2.sql
@@ -0,0 +1,28 @@
+﻿create table Cars(
+Id int primary key identity(1,1),BrandId int, ColorId int, ModelYear varchar(30), 
+DailyPrice decimal, Descriptions varchar(50), 
+Foreign Key (BrandId) references Colors(ColorId),
+Foreign Key (ColorId) references Brands(BrandId)
+
+
+)
+
+create table Colors(
+ColorId int primary key identity(1,1),
+ColorName varchar(30)
+
+)
+
+create table Brands(
+BrandId int primary key identity(1,1),
+BrandName varchar(30)
+
+)
+
+insert into Cars(BrandId,ColorId,ModelYear,DailyPrice,Descriptions) 
+Values 
+('1','2','1903','200','Manuel'),
+('2','3','1997','150','Benzin'),
+('3','5','2020','300','LPG');
+insert into Colors(ColorName) Values ('siyah','beyaz','mor');
+insert into Brands(BrandName) Values ('Kia','MiniCooper','Jeep');
